5d48aa9eed3c331bca099ee485464bd883a8c8b0
Kiryl Miarkulau <miarkulau@miarkulau.instinctools.ru>
2019-02-15 17:14:47 +0300

    feature: [ULCC-4] Refactor sagas imports / untracked file fix

--- src/sagas/root.js
+++ src/sagas/root.js
@@ -1,17 +1,17 @@
 import { spawn } from 'redux-saga/effects';
 
-import changeSettingSaga from '../sagas/changeSettingsSaga';
-import fetchSettingsSaga from '../sagas/fetchSettingsSaga';
-import fetchAuthKeySaga from '../sagas/fetchAuthKeySaga';
-import fetchCarLotsSaga from '../sagas/fetchCarLotsSaga';
-import fetchHouseLotsSaga  from '../sagas/fetchHouseLotsSaga';
-import udateWatchHouseLotsSaga from '../sagas/udateWatchHouseLotsSaga';
+import changeSettingSaga from './changeSettingsSaga';
+import fetchSettingsSaga from './fetchSettingsSaga';
+import fetchAuthKeySaga from './fetchAuthKeySaga';
+import fetchCarLotsSaga from './fetchCarLotsSaga';
+import fetchHouseLotsSaga from './fetchHouseLotsSaga';
+import udateWatchHouseLotsSaga from './udateWatchHouseLotsSaga';
 
 export default function* rootSaga() {
-    yield spawn(changeSettingSaga)
-    yield spawn(fetchSettingsSaga)
-    yield spawn(fetchAuthKeySaga)
-    yield spawn(fetchCarLotsSaga)
-    yield spawn(fetchHouseLotsSaga)
-    yield spawn(udateWatchHouseLotsSaga)
-  }
+  yield spawn(changeSettingSaga);
+  yield spawn(fetchSettingsSaga);
+  yield spawn(fetchAuthKeySaga);
+  yield spawn(fetchCarLotsSaga);
+  yield spawn(fetchHouseLotsSaga);
+  yield spawn(udateWatchHouseLotsSaga);
+}
