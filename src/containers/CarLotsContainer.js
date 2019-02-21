f2395e398060cc1f508d13560067bb5f2497c699
Kiryl Miarkulau <miarkulau@miarkulau.instinctools.ru>
2019-02-21 16:25:46 +0300

    feature: [ULCC-11] Filter screen / routers test2

--- src/containers/CarLotsContainer.js
+++ src/containers/CarLotsContainer.js
@@ -14,8 +14,119 @@ class CarLotsContainer extends PureComponent {
   }
 
   componentDidMount() {
-    const { onFetchCarLots, page, itemsPerPage } = this.props;
-    onFetchCarLots(page, itemsPerPage);
+    const {
+      onFetchCarLots, page, itemsPerPage,
+    } = this.props;
+    const filtersMock = [
+      {
+        id: 'Model',
+        label: 'Марка',
+        children: {
+          selects: {
+            fromToSelects: [],
+            nestedSelects: [
+              {
+                id: 'Mark',
+                options: ['Все марки', 'Toyota', 'Audi', 'Mercedes', 'BMW', 'Honda', 'VAZ', 'Valve'],
+                value: 'Все марки',
+                label: '',
+              },
+              {
+                id: 'PriceTo',
+                options: ['Все модели', 'Модель1', 'Модель2', 'Модель3', 'Модель4', 'Модель5'],
+                value: 'Все модели',
+                label: '',
+              },
+            ],
+            singleSelects: [],
+          },
+          buttons: [],
+          checkboxes: [],
+          inputs: [],
+        },
+      },
+      {
+        id: 'Price',
+        label: 'Цена',
+        children: {
+          selects: {
+            fromToSelects: [
+              {
+                id: 'PriceFrom',
+                options: ['Любая', '1000', '2000', '3000', '4000', '5000', '10000'],
+                value: 'Любая',
+                label: 'От',
+              },
+              {
+                id: 'PriceTo',
+                options: ['Любая', '1000', '2000', '3000', '4000', '5000', '10000'],
+                value: 'Любая',
+                label: 'До',
+              },
+            ],
+            nestedSelects: [],
+            singleSelects: [],
+          },
+          buttons: [
+            {
+              id: 'Exchange',
+              options: ['BYN', 'USD', 'EUR'],
+              value: 'BYN',
+              label: 'Валюта',
+            },
+          ],
+          checkboxes: [
+            {
+              id: 'IsExchangeble',
+              options: null,
+              value: false,
+              label: 'Обмен',
+            },
+            {
+              id: 'WithDiagnostic',
+              options: null,
+              value: false,
+              label: 'С диагностикой',
+            },
+          ],
+          inputs: [],
+        },
+      },
+      {
+        id: 'Placeholder',
+        label: 'Местонахождение',
+        children: {
+          selects: {
+            fromToSelects: [],
+            nestedSelects: [
+              {
+                id: 'Country',
+                options: ['Все страны', 'Беларусь (жыве!)', 'Россия (слава руси!)', 'Украина (слава героям!)', 'Казахстан(уважайте)', 'США (fuck yeah!)'],
+                value: 'Все страны',
+                label: 'Страна',
+              },
+              {
+                id: 'Region',
+                options: ['Все области', 'Пока все одинаковые', 'Пока все одинаковые', 'Пока все одинаковые', 'Пока все одинаковые', 'Пока все одинаковые'],
+                value: 'Все области',
+                label: 'Область',
+              },
+              {
+                id: 'Town',
+                options: ['Все области', 'Пока все одинаковые', 'Пока все одинаковые', 'Пока все одинаковые', 'Пока все одинаковые'],
+                value: 'Все области',
+                label: 'Город',
+              },
+            ],
+            singleSelects: [],
+          },
+          buttons: [],
+          checkboxes: [],
+          inputs: [],
+        },
+      },
+    ];
+    onFetchCarLots(page, itemsPerPage, filtersMock);
   }
 
   handleScrollEnd() {
@@ -52,7 +163,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    onFetchCarLots: (page, itemsPerPage) => dispatch(actions.carLotsActions.fetchCarLots({ page, itemsPerPage })),
+    onFetchCarLots: (page, itemsPerPage, filtersMock) => dispatch(actions.carLotsActions.fetchCarLots({ page, itemsPerPage, filtersMock })),
   };
 }
 
