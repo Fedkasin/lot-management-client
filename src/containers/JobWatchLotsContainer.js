8101bcf6b4045575e9f27c9deb3a3508c100cb3c
k1r1k1 <kirillmerkulov@tut.by>
2019-06-11 10:23:20 +0300

    Pull-to-refresh-fetchers-add

--- src/containers/JobWatchLotsContainer.js
+++ src/containers/JobWatchLotsContainer.js
@@ -1,26 +1,38 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import {
-  FlatList,
-  ActivityIndicator,
-} from 'react-native';
+import { FlatList } from 'react-native';
 import PropTypes from 'prop-types';
+import actions from '../store/actions';
 
 import HouseLotCard from '../components/house/HouseLotCard';
 import BgMessage from '../components/bgmessage/BackgroundMessage';
-import * as Colors from '../constants/Colors';
 import * as Errors from '../constants/Errors';
 
 class JobWatchLotsContainer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.fetchItems = this.fetchItems.bind(this);
+  }
+
+  fetchItems() {
+    const { onUpdateItems, jobId } = this.props;
+    onUpdateItems(jobId);
+  }
+
   render() {
-    const { isFetching, houseWatchLots, error } = this.props;
-    if (!houseWatchLots.length && isFetching) return <ActivityIndicator size="large" color={Colors.lightGray} />;
+    const {
+      isFetching,
+      houseWatchLots,
+      error,
+    } = this.props;
+
     return (
       <FlatList
         data={houseWatchLots}
         renderItem={({ item }) => <HouseLotCard item={item} />}
         keyExtractor={item => item.id.toString()}
         onEndReached={this.handleScrollEnd}
+        onRefresh={this.fetchItems}
         onEndReachedThreshold={0}
         refreshing={isFetching}
         ListEmptyComponent={() => <BgMessage text={error || Errors.notfound} />}
@@ -34,6 +46,13 @@ function mapStateToProps(state) {
     isFetching: state.houseWatchLotsReducers.isFetching,
     houseWatchLots: state.houseWatchLotsReducers.houseWatchLots,
     error: state.houseWatchLotsReducers.error ? state.houseWatchLotsReducers.error : null,
+    jobId: state.houseWatchLotsReducers.jobId,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onUpdateItems: jobId => dispatch(actions.houseWatchLotsActions.updateHouseWatchLots(jobId)),
   };
 }
 
@@ -41,10 +60,12 @@ JobWatchLotsContainer.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   houseWatchLots: PropTypes.arrayOf(PropTypes.any).isRequired,
   error: PropTypes.string,
+  jobId: PropTypes.string.isRequired,
+  onUpdateItems: PropTypes.func.isRequired,
 };
 
 JobWatchLotsContainer.defaultProps = {
   error: '',
 };
 
-export default connect(mapStateToProps)(JobWatchLotsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(JobWatchLotsContainer);
