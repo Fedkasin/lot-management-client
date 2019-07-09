9387f78fe3d66a4c92ea58d196be5d885acc9429
Siniauski Fiodar <fiodar.siniauski@colabpro.com>
2019-07-09 15:21:18 +0300

    revert: old settings

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,16 +1,15 @@
 export const googleAuthorizationConfig = {
-  iosClientId: '174973245157-kcjai6nva876o7bgd22928e3n1n5fj1n.apps.googleusercontent.com',
-  androidClientId: '174973245157-uaumjt592jg2gb0eidsmercprimckgsg.apps.googleusercontent.com',
+  iosClientId: '703291837548-8ag78bfhrrvdskm5qvcg88406bbsqsig.apps.googleusercontent.com',
+  androidClientId: '703291837548-7b0s1k4i9e9c0l8cko5n8oe4f0shrb6p.apps.googleusercontent.com',
   scopes: ['profile', 'email'],
   behavior: 'web',
 };
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDcGAr8FIrnHJhi3q_mkmmJhVQ_-uQtFSw',
-  authDomain: 'coral-airlock-245512.firebaseapp.com',
-  databaseURL: 'https://coral-airlock-245512.firebaseio.com',
-  projectId: 'coral-airlock-245512',
-  storageBucket: '',
-  messagingSenderId: '174973245157',
-  appId: '1:174973245157:web:b0696c771eb53fb5',
+  apiKey: 'AIzaSyCFkoOZb16br6NZTQxbV38X7rFQ8eITgfs',
+  authDomain: 'lot-management-client.firebaseapp.com',
+  databaseURL: 'https://lot-management-client.firebaseio.com',
+  projectId: 'lot-management-client',
+  storageBucket: 'lot-management-client.appspot.com',
+  messagingSenderId: '703291837548',
 };
